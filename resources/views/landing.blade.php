02b69d26e9508ad9f4d05180579146684d02aa85
Sunny Bharadwaj <sunny@chakradesign.co>
2019-11-23 16:54:31 +0530

    Finished splash page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description"
+          content="Pratham is a People Development organisation, established in 2010, is based out of Hyderabad with operations across the county. Our focus is on the behavioural aspect of people skill. We closely work with Corporate and Educational Institutes.">
+    <meta name="keywords"
+          content="Diversity, Inclusion, Workplace happiness, Assessment, Behavioral, Potential, Mentoring, Leadership, Bespoke, Coaching, Corporate, Empower, Professional, Development, Transform, Training, Organisations">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Pratham | We Transform People & Organisations</title>
+</head>
+<body>
+<div class="mb-4"></div>
+<div class="max-w-xl mx-auto text-center">
+    <img class="mx-auto" src="/static/logos/logo_main.png" alt="">
+</div>
+
+<div class="center-body max-w-2xl mx-auto text-center py-16">
+    <h1 class="h1 mb-1">Under Maintenance!</h1>
+    <h2 class="h2 mb-8">Our website is undergoing some major changes. We will be back soon with an updated experience.</h2>
+
+    <a class="link" href="/static/documents/Pratham - We Transform People.pdf" target="_blank">Download our Pitch Deck</a>
+</div>
+
+<footer class="text-center py-12">
+    <p class="h2 mb-2">
+        Meanwhile, to <span class="footer-highlight h2 usable-green font-bold">Contact us</span>
+    </p>
+
+    <div>
+        <p class="inline-block emp mr-6">Sudhir Gupta</p>
+        <p class="inline-block emp mr-6">90141 02000‬</p>
+        <p class="inline-block emp">sudhir@prathamtrainers.co</p>
+    </div>
+
+
+
+</footer>
+</body>
+</html>
